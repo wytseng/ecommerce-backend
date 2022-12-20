bce6a75147b8a4567d7d68ba43b871f8d8d28c1b
Steph T <wytseng@uci.edu>
2022-12-20 14:52:54 -0800

    successfully connected to dbserver

--- models/Category.js
+++ models/Category.js
@@ -13,7 +13,7 @@ Category.init(
       primaryKey: true,
       autoIncrement: true
     },
-    catergory_name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false
     }
